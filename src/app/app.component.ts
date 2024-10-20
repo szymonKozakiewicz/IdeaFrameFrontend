8c9a863605e70f213acd581a0a7c7fc4d438b87f
szymon <sz.kozakiewicz@gmail.com>
2024-10-20 20:15:03 +0200

    root module works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ideaFrame';
+  title = 'ideaFram';
 }
---
+++ src/app/root/root.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from '../app.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  bootstrap:[AppComponent]
+})
+export class RootModule { }
+
