fc1171165672116e62a0343df948dfd201f7ccab
szymon <sz.kozakiewicz@gmail.com>
2024-10-21 18:30:10 +0200

    add routing module and rename root module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from '../app.component';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,AppRoutingModule],
   bootstrap:[AppComponent]
 })
-export class RootModule { }
+export class AppModule { }
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {RootModule} from "./app/root/root.module";
-platformBrowserDynamic().bootstrapModule(RootModule)
+import {AppModule} from "./app/app.module";
+platformBrowserDynamic().bootstrapModule(AppModule)
