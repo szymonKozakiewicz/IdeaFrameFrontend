38c008bfc8a3aa3e00ecfb50e623ae15fedcac93
szymon <sz.kozakiewicz@gmail.com>
2024-10-24 21:27:41 +0200

    add logo component, fonts and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LogoComponent } from './logo/logo.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,LogoComponent,WelcomeComponent],
   imports: [BrowserModule,AppRoutingModule],
   bootstrap:[AppComponent]
 })
