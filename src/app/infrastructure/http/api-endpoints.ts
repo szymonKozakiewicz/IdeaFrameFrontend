82329a4ae8bf4b4ab7f9649509c0cc633ceb0e37
szymon <sz.kozakiewicz@gmail.com>
2025-01-16 14:08:32 +0100

    adjust endpoints to splited register login controller

--- src/app/infrastructure/http/api-endpoints.ts
+++ src/app/infrastructure/http/api-endpoints.ts
@@ -1,10 +1,10 @@
 export class ApiEndpoints {
     private static readonly BASE_URL = "https://localhost:7231"
 
-    static readonly REFRESH_TOKEN = `${ApiEndpoints.BASE_URL}/api/RegisterLogin/refreshToken`;
-    static readonly LOGIN = `${ApiEndpoints.BASE_URL}/api/RegisterLogin/login`;
-    static readonly REGISTER = `${ApiEndpoints.BASE_URL}/api/RegisterLogin/registerNewUser`;
-    static readonly IS_LOGIN_AVAILABLE = `${ApiEndpoints.BASE_URL}/api/RegisterLogin/isLoginAvailable`;
+    static readonly REFRESH_TOKEN = `${ApiEndpoints.BASE_URL}/api/Login/refreshToken`;
+    static readonly LOGIN = `${ApiEndpoints.BASE_URL}/api/Login/login`;
+    static readonly REGISTER = `${ApiEndpoints.BASE_URL}/api/Register/registerNewUser`;
+    static readonly IS_LOGIN_AVAILABLE = `${ApiEndpoints.BASE_URL}/api/Register/isLoginAvailable`;
     static readonly AUTHORIZED_REQUEST = `${ApiEndpoints.BASE_URL}/api/UserPanel/authorizedRequest`;
-    static readonly LOGOUT = `${ApiEndpoints.BASE_URL}/api/RegisterLogin/logout`;
+    static readonly LOGOUT = `${ApiEndpoints.BASE_URL}/api/Login/logout`;
   }
