f1c5e9e72bcf1ab1bcadcab556b435b59bd1f73c
szymon <sz.kozakiewicz@gmail.com>
2025-05-24 17:59:07 +0200

    add test for dragging node

--- src/app/presentation/mindmap-panel/mind-map/mind-map.component.spec.ts
+++ src/app/presentation/mindmap-panel/mind-map/mind-map.component.spec.ts
@@ -5,6 +5,13 @@ import { LogoComponent } from '../../logo/logo.component';
 import { getMindMapMock } from 'src/app/testHelpers/service-mock-generator';
 import { MindMapService } from 'src/app/core/services/mind-map.service';
 import { FormsModule } from '@angular/forms';
+import { getElementByTestId } from 'src/app/testHelpers/data-testid-selector';
+import { NodeCoordinates } from 'src/app/core/domain/entities/node-coordinates';
+import { min } from 'rxjs';
+import { NodeMindMap } from 'src/app/core/domain/entities/node-mind-map';
+import { By } from '@angular/platform-browser';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { NodeComponent } from './node/node.component';
 
 describe('MindMapComponent', () => {
   let component: MindMapComponent;
@@ -14,11 +21,11 @@ describe('MindMapComponent', () => {
   beforeEach(async () => {
       mindMapServiceMock=getMindMapMock();
     await TestBed.configureTestingModule({
-      declarations: [MindMapComponent,LogoComponent],
+      declarations: [MindMapComponent,LogoComponent,NodeComponent],
       providers: [
         {provide: MindMapService ,useValue: mindMapServiceMock}
       ],
-      imports: [FormsModule]
+      imports: [FormsModule,CdkDrag, CdkDropList]
     })
     .compileComponents();
 
@@ -30,4 +37,32 @@ describe('MindMapComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('after dragEnd event happend method updateSelectedNodePosition should be called', () => {
+    let testNodeCordinates:NodeCoordinates=new NodeCoordinates(100,200);
+    let testNode:NodeMindMap=new NodeMindMap("testNode","testNodeId","#fffaf0",testNodeCordinates,false);
+    mindMapServiceMock.getNodes.and.returnValue([testNode]);
+    mindMapServiceMock.mindMapUpdated$.next();
+    fixture.detectChanges();
+
+    let elementNode=getElementByTestId(fixture,"node");
+    const nodeElement = fixture.debugElement.query(By.css('[data-testid="node"]'));
+    const dragDirective = fixture.debugElement.query(By.directive(CdkDrag));
+    const dragRef = dragDirective.injector.get(CdkDrag);
+    const mockDragEndEvent = {
+      source: dragRef,
+      distance: { x: 100, y: 200 },
+      dropPoint: { x: 100, y: 200 }
+    };
+
+    //act
+    nodeElement.triggerEventHandler('cdkDragEnded', mockDragEndEvent);
+    fixture.detectChanges();
+
+    //assert
+    expect(mindMapServiceMock.updateSelectedNodePosition).toHaveBeenCalled();
+
+    
+  })
+
 });
